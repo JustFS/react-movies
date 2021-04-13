467184f8a48fdd4c10541234fbeadbb1701ed8e4
Julien Az <julien.azbrg@gmail.com>
2021-04-13 19:43:27 +0200

    index

--- src/App.js
+++ src/App.js
@@ -3,17 +3,17 @@ import axios from "axios";
 import MoviesCard from "./components/MoviesList/MoviesCard";
 
 function App() {
-  const [movies, setMovies] = useState();
-  const [input, setInput] = useState();
-  const [search, setSearch] = useState();
+  const [movies, setMovies] = useState([]);
+  const [search, setSearch] = useState("");
 
   useEffect(() => {
+    console.log(movies.length);
     if (search) {
       axios
         .get(
           `https://api.themoviedb.org/3/search/movie?api_key=2a7bc3506d9e237e752a6e713962fea0&query=${search}`
         )
-        .then((resp) => setMovies(resp.data.results));
+        .then((res) => setMovies(res.data.results));
     }
   }, [search]);
 
@@ -21,28 +21,21 @@ function App() {
     <div className="App">
       <h1>AlloMovies</h1>
       <div className="search">
-        <form
-          onSubmit={(e) => {
-            e.preventDefault();
-            setSearch(input);
-          }}
-        >
+        <form onSubmit={(e) => e.preventDefault()}>
           <input
             className="inp-search"
             type="text"
             placeholder="Entrez un film"
             defaultValue=""
-            onChange={(e) => setInput(e.target.value)}
+            onChange={(e) => setSearch(e.target.value)}
           />
           <button type="submit">Rechercher</button>
         </form>
       </div>
 
       <div className="result">
-        {movies &&
-          movies.map((movie, index) => (
-            <MoviesCard key={index} movie={movie} />
-          ))}
+        {search !== "" &&
+          movies.map((movie) => <MoviesCard movie={movie} key={movie.id} />)}
       </div>
     </div>
   );
--- src/components/MoviesList/MoviesCard.js
+++ src/components/MoviesList/MoviesCard.js
@@ -1,19 +1,17 @@
 import React, { useState } from "react";
 import "./movieCard.scss";
 
-const MoviesCard = ({ movie }) => {
-  const [data] = useState(movie);
+const MoviesCard = (props) => {
+  const [movie] = useState(props.movie);
   return (
     <div className="moviesCard">
-      {data.poster_path && (
-        <img
-          className="img"
-          src={`https://image.tmdb.org/t/p/w200${data.poster_path}`}
-          alt=""
-        />
-      )}
-      <h2>{data.original_title}</h2>
-      <p>{data.overview}</p>
+      <img
+        className="img"
+        src={`https://image.tmdb.org/t/p/w200${movie.poster_path}`}
+        alt=""
+      />
+      <h2>{movie.original_title}</h2>
+      <p>{movie.overview}</p>
     </div>
   );
 };
